2ffdd5003b2c747be00b201ec125184aec5fddf0
Simon <sawsimonelinn@gmail.com>
2024-06-06 00:26:15 -0700

    adding main content

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './WeatherCard.css';
+import sunny from '../../assets/sunny.png';
 
 const WeatherCard = () => {
-  return <div>WeatherCard</div>;
+  return (
+    <section className='weather-card'>
+      <p className='weather-card__temp'>75 &deg; F</p>
+      <img src={sunny} alt='' className='weather-card__image' />
+    </section>
+  );
 };
 
 export default WeatherCard;
